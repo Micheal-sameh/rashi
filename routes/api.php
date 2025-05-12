dd7a148827500ed4bdb057f431b0730f7af747a6
micheal <micheal.s.samir@gmail.com>
2025-05-12 19:44:55 +0300

    swagegr

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/swagger', function () {
+    // Define which documentation to use, defaulting to 'default' if not set
+    $documentation = config('l5-swagger.documentation') ?? 'default';
+    $useAbsolutePath = true;
+
+    return view('vendor.l5-swagger.index', compact('documentation', 'useAbsolutePath'));
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
