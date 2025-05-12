3d71a796462dfbc67e7a48362f117cf37aca0353
micheal <micheal.s.samir@gmail.com>
2025-05-12 19:51:50 +0300

    base controller

---
+++ app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ResponseHandler;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class BaseController extends Controller implements HasMedia
+{
+    use InteractsWithMedia;
+    use ResponseHandler;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function apiResponse($data = null, $message = null, $status_code = 200, $additional_data = null)
+    {
+        return response()->json([
+            'status_code' => is_array($status_code) ? $status_code[1] : $status_code,
+            'message' => $message ?: trans('messages.success'),
+            'data' => $data,
+            'additional_data' => $additional_data,
+        ]);
+    }
+
+    public function apiErrorResponse($message = null, $status_code = 400)
+    {
+        return response()->json([
+            'status_code' => is_array($status_code) ? $status_code[1] : $status_code,
+            'message' => $message ?: trans('messages.bad_request'),
+        ], is_array($status_code) ? $status_code[0] : $status_code);
+    }
+}
