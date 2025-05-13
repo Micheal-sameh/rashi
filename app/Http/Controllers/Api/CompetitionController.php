030f0dfecdb5e9a0541c6a4c613bf1c641ec7880
micheal <micheal.s.samir@gmail.com>
2025-05-13 21:52:51 +0300

    index in api

--- app/Http/Controllers/Api/CompetitionController.php
+++ app/Http/Controllers/Api/CompetitionController.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\CompetitionResource;
+use App\Services\CompetitionService;
+
 class CompetitionController extends BaseController
 {
-    public function index() {}
+    public function __construct(protected CompetitionService $competitionService) {}
+
+    public function index()
+    {
+        $competitions = $this->competitionService->index();
+
+        return $this->respondResource(CompetitionResource::collection($competitions));
+    }
 }
