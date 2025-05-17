ec92883178e2452f1e80331e1ff2c1e07b9d79b5
micheal <micheal.s.samir@gmail.com>
2025-05-17 20:29:10 +0300

    settings seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolesAndPermissionSeeder::class);
+        $this->call(SettingDatabaseSeeder::class);
     }
 }
