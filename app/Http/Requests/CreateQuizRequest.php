23de6f61d98eb17f71f765d6d650e0ed6e1365fd
micheal <micheal.s.samir@gmail.com>
2025-10-22 13:52:34 +0300

    validate time range when submmiting create new quiz

--- app/Http/Requests/CreateQuizRequest.php
+++ app/Http/Requests/CreateQuizRequest.php
@@ -10,7 +10,26 @@ class CreateQuizRequest extends FormRequest
     {
         return [
             'name' => 'required|string',
-            'date' => 'required|date|after_or_equal:today',
+            'date' => [
+                'required',
+                'date',
+                'after_or_equal:today',
+                function ($attribute, $value, $fail) {
+                    $competitionId = $this->input('competition_id');
+                    if ($competitionId) {
+                        $competition = \App\Models\Competition::find($competitionId);
+                        if ($competition) {
+                            $quizDate = \Carbon\Carbon::parse($value);
+                            $startDate = \Carbon\Carbon::parse($competition->start_at);
+                            $endDate = \Carbon\Carbon::parse($competition->end_at);
+
+                            if ($quizDate->lt($startDate) || $quizDate->gt($endDate)) {
+                                $fail('The quiz date must be between the competition start date ('.$startDate->format('Y-m-d').') and end date ('.$endDate->format('Y-m-d').').');
+                            }
+                        }
+                    }
+                },
+            ],
             'competition_id' => 'required|integer|exists:competitions,id',
             'questions' => 'required|array',
             'questions.*.question' => 'required|string',
