478fafff29c71c730b3e79f1fe5925f408a1bb24
micheal <micheal.s.samir@gmail.com>
2025-11-23 12:20:59 +0200

    add show result in resource of quiz questions

--- app/Http/Controllers/Api/QuizQuestionController.php
+++ app/Http/Controllers/Api/QuizQuestionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\QuestionIndexRequest;
 use App\Http\Resources\QuizQuestionResource;
 use App\Services\QuizQuestionService;
+use Carbon\Carbon;
 
 class QuizQuestionController extends BaseController
 {
@@ -17,6 +18,7 @@ class QuizQuestionController extends BaseController
         return $this->respondResource(QuizQuestionResource::collection($questions),
             additional_data: [
                 'quiz_have_solved' => ! $questions->first()->quiz->isSolved->isEmpty(),
+                'show_results' => (Carbon::parse($questions->first()->quiz->date)->lt(today())),
             ]
         );
     }
