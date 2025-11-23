0804c05953e51f22d06780111260f00284f52386
micheal <micheal.s.samir@gmail.com>
2025-11-23 12:09:52 +0200

    add is_correct in the resource

--- app/Http/Resources/QuizQuestionResource.php
+++ app/Http/Resources/QuizQuestionResource.php
@@ -19,6 +19,7 @@ class QuizQuestionResource extends JsonResource
             'id' => $this->id,
             'question' => $this->question,
             'points' => $this->points,
+            'is_correct' => (Carbon::parse($this->quiz->date)->lt(today())) ? true : false,
             'answers' => (Carbon::parse($this->quiz->date)->lt(today()))
             ? $this->whenloaded('answers', ModelAnswerResource::collection($this->answers))
             : $this->whenloaded('answers', AnswerResource::collection($this->answers)),
